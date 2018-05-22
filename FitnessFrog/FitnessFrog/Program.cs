983dbc174706ec75fbd0542e876eb8bb1eb51aa6
MattHall2 <MattHall2@users.noreply.github.com>
2018-05-22 08:40:46 -0400

    Complete Application

---
+++ FitnessFrog/FitnessFrog/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FitnessFrog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.Write("How many minutes have you exercised today: ");
+                
+                string entry = Console.ReadLine();
+                
+                if (entry == "quit")
+                {
+                    
+                    keepGoing = false;
+                }
+                else
+                {
+                    try
+                    {
+                        int minutes = int.Parse(entry);
+
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                        }
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("Thats a good start, next time try 15 minutes.");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Thats great!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You reached the peak and kept going!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Your going to pull something!");
+                        }
+                        runningTotal = runningTotal + minutes;
+
+                        Console.WriteLine("You've exercised " + runningTotal + " minutes");
+                        Console.WriteLine("\nType \"quit\" to exit");
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("The entry is not a valid input.");
+                        continue;
+                    }
+                   
+
+                    
+                }
+
+
+                
+
+
+            } 
+        }
+    }
+}
